475e1ded6a98bcf008f41dedec1c26710bc67ae9
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-03-05 20:18:18 +0500

    Route connects to SiteController.php file

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 // Route::get('/about', function () {
 //     return view('about');
 // });
+
+
 
 
 Route::get('/', function () {
